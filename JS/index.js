1b2fe47e133ee6ba464ebc01ca7bbb86fba53314
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-20 17:57:07 +0200

    first clicked feature aproach  working

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+console.log('hello kitty');
+
+let clicks = 0;
+
+let btn = document.createElement("button");
+btn.textContent = "KISSED";
+document.body.appendChild(btn);
+
+btn.addEventListener("click", function () {
+    clicks++;
+    console.log(clicks);
+    btn.textContent = "KISSED: " + clicks;
+
+});
+
+
