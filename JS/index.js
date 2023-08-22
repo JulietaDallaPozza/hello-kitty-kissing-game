8ded78b043c361c2c0cf48684d342299281c90e7
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-22 12:52:51 +0200

    wip

--- JS/index.js
+++ JS/index.js
@@ -1,63 +1,59 @@
-// console.log('hello kitty');
+console.log('hello kitty');
 
 
 
 
-// // // Function to create a new Hello Kitty head element
-// // function createHelloKitty() {
-// //   const kittyContainer = document.createElement("div");
-// //   kittyContainer.classList.add("container");
-// //   // Copy the Hello Kitty HTML structure into the new container
-// //   kittyContainer.innerHTML = document.querySelector(".container").innerHTML;
-// //   // Apply styles to the container, e.g., position, random rotation, etc.
-// //   kittyContainer.style.position = "absolute";
-// //   kittyContainer.style.transform = `rotate(${Math.random() * 360}deg)`;
+// Function to create hello Kitty head element
 
-// //   // Add event listener to remove the kitty when clicked
-// //   kittyContainer.addEventListener("click", handleKittyClick);
+function createHelloKitty() {
+  const kittyContainer = document.createElement("div");
+  kittyContainer.classList.add("container");
+  kittyContainer.innerHTML = document.querySelector(".container").innerHTML;
+  kittyContainer.style.position = "absolute";
+  kittyContainer.style.transform = `rotate(${Math.random() * 360}deg)`;
 
-// //   // Return the new Hello Kitty container
-// //   return kittyContainer;
-// // }
+  kittyContainer.addEventListener("click", handleKittyClick);
 
-// // // Function to handle Kitty click event
-// // function handleKittyClick(event) {
-// //   const kitty = event.currentTarget;
-// //   // Add your interaction logic here (e.g., score increment, animation, etc.)
-// //   // Remove the kitty element
-// //   kitty.remove();
-// // }
+  return kittyContainer;
+}
 
-// // Function to start the falling animation for a kitty
-// function startFalling(kitty) {
-//   const container = document.getElementById("game-container");
-//   container.appendChild(kitty);
+function handleKittyClick(event) {
+  const kitty = event.currentTarget;
+ 
+  kitty.remove();
+}
 
-//   const startPosition = Math.random() * (window.innerWidth - 100);
-//   const startRotation = Math.random() * 360;
-//   const duration = Math.random() * 4000 + 2000;
+// function to start the falling animation for a kitty
 
-//   kitty.style.left = `${startPosition}px`;
-//   kitty.style.transform = `rotate(${startRotation}deg)`;
+function startFalling(kitty) {
+  const container = document.getElementById("game-container");
+  container.appendChild(kitty);
 
-//   kitty.animate(
-//     [
-//       { top: "-100px", opacity: 1 },
-//       { top: `${window.innerHeight}px`, opacity: 0 },
-//     ],
-//     {
-//       duration: duration,
-//       easing: "ease-in-out",
-//     }
-//   ).onfinish = () => {
-//     kitty.remove();
-//   };
-// }
+  const startPosition = Math.random() * (window.innerWidth - 100);
+  const startRotation = Math.random() * 360;
+  const duration = Math.random() * 4000 + 2000;
 
-// // Start generating falling kitties
-// setInterval(() => {
-//   const newKitty = createHelloKitty();
-//   startFalling(newKitty);
-// }, 1000);
+  kitty.style.left = `${startPosition}px`;
+  kitty.style.transform = `rotate(${startRotation}deg)`;
+
+  kitty.animate(
+    [
+      { top: "-100px", opacity: 1 },
+      { top: `${window.innerHeight}px`, opacity: 0 },
+    ],
+    {
+      duration: duration,
+      easing: "ease-in-out",
+    }
+  ).onfinish = () => {
+    kitty.remove();
+  };
+}
+
+// start generating falling kitties
+setInterval(() => {
+  const newKitty = createHelloKitty();
+  startFalling(newKitty);
+}, 1000);
 
 
--- JS/script.js
+++ JS/script.js
@@ -28,7 +28,6 @@
 
 window.onload = function () {
     const startButton = document.getElementById("start-button");
-    let setInterval; // To store the interval for generating falling kitties
 
     startButton.addEventListener("click", function () {
         start();
@@ -36,67 +35,10 @@ window.onload = function () {
 
     function start() {
         console.log("KISS");
-        startButton.disabled = true; // Disable the start button
+        startButton.disabled = true; // disable the start button
 
 
-        // Function to create a new Hello Kitty head element
-        function createHelloKitty() {
-            const kittyContainer = document.createElement("div");
-            kittyContainer.classList.add("container");
-            // Copy the Hello Kitty HTML structure into the new container
-            kittyContainer.innerHTML = document.querySelector(".container").innerHTML;
-            // Apply styles to the container, e.g., position, random rotation, etc.
-            kittyContainer.style.position = "absolute";
-            kittyContainer.style.transform = `rotate(${Math.random() * 360}deg)`;
-
-            // Add event listener to remove the kitty when clicked
-            kittyContainer.addEventListener("click", handleKittyClick);
-
-            // Return the new Hello Kitty container
-            return kittyContainer;
-        }
-
-        function startFalling(kitty) {
-            const container = document.getElementById("game-container");
-            container.appendChild(kitty); 
-        
-            kitty.style.left = `${startPosition}px`;
-            kitty.style.transform = `rotate(${startRotation}deg)`;
-
-            kitty.animate(
-                [
-                    { top: "-100px", opacity: 1 },
-                    { top: `${window.innerHeight}px`, opacity: 0 },
-                ],
-                {
-                    duration: duration,
-                    easing: "ease-in-out",
-                }
-            ).onfinish = () => {
-                kitty.remove();
-            };
-        }
-            // Function to handle Kitty click event
-            function handleKittyClick(event) {
-                const kitty = event.currentTarget;
-                // Add your interaction logic here (e.g., score increment, animation, etc.)
-                // Remove the kitty element
-                kitty.remove();
-            }
-
-
-
-            // Rest of your existing code for handling kitty clicks and falling animation
-
-            // Generate kitties and start falling animation
-
-
-
-            // / Start generating falling kitties
-            setInterval(() => {
-                const newKitty = createHelloKitty();
-                startFalling(newKitty);
-            }, 1000);
+    
         }
 
 
