723b19982d81367121262b9e7aa1b9767955b01b
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-23 11:23:30 +0200

    score done

--- JS/game.js
+++ JS/game.js
@@ -1,81 +1,54 @@
-
-
-
 class Game {
     constructor() {
-        this.startScreen = document.querySelector("#game-intro"); //holds the div element #game-intro. To access the element, use either document.getElementById() or document.querySelector().
+        this.startScreen = document.querySelector("#game-intro");
         this.gameScreen = document.querySelector("#game-screen");
         this.displayScore = document.querySelector("#score");
         this.gameEndScreen = document.querySelector("#game-end");
-
-        console.log(this.displayScore)
-
         this.clicks = 0;
         this.height = 300;
         this.width = 200;
         this.score = 0;
-
-
     }
 
     start() {
         console.log("started");
-
         this.gameScreen.style.height = `${this.height}px`;
         this.gameScreen.style.width = `${this.width}px`;
         this.startScreen.style.display = "none";
         this.gameScreen.style.display = "block";
-        // this.gameLoop();
+        this.gameLoop();
     }
 
     gameLoop() {
-
-
         // this.startScreen.style.display = "none";
         // this.gameScreen.style.display = "none";
 
+        const self = this;
         function createHelloKitty() {
             const kittyContainer = document.createElement("div");
             kittyContainer.classList.add("container");
             kittyContainer.innerHTML = document.querySelector(".container").innerHTML;
             kittyContainer.style.position = "absolute";
             kittyContainer.style.transform = `rotate(${Math.random() * 360}deg)`;
-
-            kittyContainer.addEventListener("click", handleKittyClick);
-
-
-
-            let clicks = 0;
-
-            kittyContainer.addEventListener("click", function () {
-                console.log(this.clicks)
-                // let displayScore = document.getElementById("score");
-                clicks++;
-                console.log(this.displayScore)
-                this.displayScore.textContent = "kISseS: "
-
-            });
-
+            kittyContainer.addEventListener("click", handleKittyClick.bind(self), false);
             return kittyContainer;
         }
 
         function handleKittyClick(event) {
+            this.clicks++;
+            this.displayScore.innerHTML = this.clicks;
             const kitty = event.currentTarget;
-
             kitty.remove();
         }
 
         function startFalling(kitty) {
             const container = document.getElementById("game-container");
             container.appendChild(kitty);
-
             const startPosition = Math.random() * (window.innerWidth - 100);
             const startRotation = Math.random() * 360;
             const duration = Math.random() * 4000 + 2000;
-
             kitty.style.left = `${startPosition}px`;
             kitty.style.transform = `rotate(${startRotation}deg)`;
-
             kitty.animate(
                 [
                     { top: "-100px", opacity: 1 },
@@ -95,10 +68,5 @@ class Game {
             const newKitty = createHelloKitty();
             startFalling(newKitty);
         }, 1000);
-
-
-
     }
-
 }
-
