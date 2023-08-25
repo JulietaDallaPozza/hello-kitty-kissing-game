947d4bf0ae786bd72ea4aaa1227f2372145c54a1
Julietadallapozza <dallapozzajulieta@gmail.com>
2023-08-25 15:02:02 +0200

    wip

--- JS/script.js
+++ JS/script.js
@@ -1,56 +1,36 @@
 
 
-window.onload = function () {
+
+
+
+
+const onTimerEnd = document.getElementById("on-timer-end"); // declare outside window.onload
+
+
+    window.onload = function () {
     const startButton = document.getElementById("start-button");
-    const playMusic = document.getElementById("play-sound");
+    const playMusic = document.getElementById("play-sound")
     const onTimerEnd = document.getElementById("on-timer-end");
+
     let game;
 
     startButton.addEventListener("click", () => {
-        game = new Game();
-        game.start();
+        game = new Game(); // inizialisate the game class when the button is clicked
+        game.start(); // start the game
         playMusic.play();
-        toggleElementVisibility(onTimerEnd, false);
+        // toggleElementVisibility(onTimerEnd, false);
+
     });
+
+};
 
     const restartButton = document.getElementById("restart-button");
     restartButton.addEventListener("click", () => {
-        game = new Game();
-        game.start();
-        restartButton.style.display = "none";
-        onTimerEnd.style.display = "none"; // Hide the on-timer-end element
-    });
-};
-
-function toggleElementVisibility(element, hide) {
-    element.style.display = hide ? "none" : "";
-}
+    game = new Game();
+    game.start();
+    restartButton.style.display = "none";
+    onTimerEnd.style.display = "none"; // Hide the on-timer-end element
 
 
-
-
-
-// window.onload = function () {
-//     const startButton = document.getElementById("start-button");
-//     const playMusic = document.getElementById("play-sound")
-//     let game;
-
-//     startButton.addEventListener("click", () => {
-//         game = new Game(); // inizialisate the game class when the button is clicked
-//         game.start(); // start the game
-
-//         playMusic.play();
-
-//     });
-
-// };
-
-// const restartButton = document.getElementById("restart-button");
-// restartButton.addEventListener("click", () => {
-//     game = new Game();
-//     game.start();
-//     restartButton.style.display = "none";
-//     // this.onTimerEnd.style.display = "none";
-//     // onTimerEnd.style.display = "none";
-// });
+});
 
